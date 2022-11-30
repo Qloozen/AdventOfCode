8bcb55746efd1db087e61bd4415ab119a93a47c8
Qiang Loozen <qiangloozen@gmail.com>
2022-11-30 21:42:51 +0100

    'init'

---
+++ 2022/day2/solution.py
@@ -0,0 +1,11 @@
+ans = 0
+ans2 = 0
+with open("day2\input.txt") as f:
+    input = f.read().splitlines()
+    # input = f.read().split("")
+    # input = [int(num) for num in f.read().splitlines()]
+    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+
+
+print(ans)
+print(ans2)
