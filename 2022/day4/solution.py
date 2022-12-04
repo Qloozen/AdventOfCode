76e5dd9679e47c83ad988b4a66db74f748b1c6bc
Qiang Loozen <qiangloozen@gmail.com>
2022-12-04 09:16:30 +0100

    day 4 - 2022

--- 2022/day4/solution.py
+++ 2022/day4/solution.py
@@ -1,11 +1,14 @@
+import re
 ans = 0
 ans2 = 0
 with open("day4\input.txt") as f:
-    input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+    input = [tuple(int(c) for c in re.split('-|,', line))
+             for line in f.read().splitlines()]
 
+for line in input:
+    a, b = set(range(line[0], line[1] + 1)), set(range(line[2], line[3] + 1))
+    ans += len(a - b) == 0 or len(b - a) == 0
+    ans2 += len(a & b) > 0
 
 print(ans)
 print(ans2)
