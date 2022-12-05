e9702dc5553b5c4d084c4340db7f9b6a6dcad63c
Qiang Loozen <qiangloozen@gmail.com>
2022-12-05 07:03:55 +0100

    partial

--- 2022/day-05/solution.py
+++ 2022/day-05/solution.py
@@ -1,11 +1,38 @@
+import re
 ans = 0
 ans2 = 0
 with open("day-05/input.txt") as f:
-    input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+    input = f.read().split('\n\n')
+    stacks_raw = input[0].splitlines()
+    instructions = [[int(num) for num in re.findall(r'\d+', line)]
+                    for line in input[1].splitlines()]
 
+    stack_list = []
+    # print(input[0])
+    for x in range(1, len(stacks_raw[0]), 4):
+        new_stack = []
+        is_stack = False
+        for y in range(0, len(stacks_raw)):
+            char = stacks_raw[y][x]
+            if (char.isalpha()):
+                new_stack.append(char)
+                is_stack = True
+        if is_stack:
+            new_stack.reverse()
+            stack_list.append(new_stack)
 
-print(ans)
-print(ans2)
+    p2_stack_list = stack_list.copy()
+    for instruction in instructions:
+        amount, from_stack, to_stack = instruction[0], instruction[1] - \
+            1, instruction[2] - 1
+
+        for i in range(0, amount):
+            stack_list[to_stack].append(stack_list[from_stack].pop())
+
+        t = p2_stack_list[from_stack][-amount:]
+        p2_stack_list[to_stack].append(p2_stack_list[from_stack][-amount:])
+
+    ans = ''
+    for stack in stack_list:
+        ans += stack.pop()
+    print(ans)
