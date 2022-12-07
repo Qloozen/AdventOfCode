02294f8618b7831bdbb42e1f357abf0218a5b825
qloozen <qiangloozen@gmail.com>
2022-12-07 09:43:36 +0100

    day 7 - 2022

--- 2022/day-07/solution.py
+++ 2022/day-07/solution.py
@@ -2,10 +2,41 @@ ans = 0
 ans2 = 0
 with open("day-07/input.txt") as f:
     input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
 
+fs = {}
+currentDir = []
+# /abcd
+for line in input:
+    parts = line.split()
+    if parts[1] == 'cd':
+        if parts[2] == '..':
+            currentDir.pop()
+        else:
+            currentDir.append(parts[2]) 
+    if parts[0].isnumeric():
+        currentDir_copy = currentDir.copy()
+        key = '/'.join(currentDir_copy)
+        fs[key] = fs.get(key, 0) + int(parts[0])
+
+        for i in range(len(currentDir_copy)):
+            currentDir_copy.pop()
+            if len(currentDir_copy) < 1:
+                continue
+            key = '/'.join(currentDir_copy)
+            fs[key] = fs.get(key, 0) + int(parts[0])
+
+total_size = fs['/']
+available_space = 70000000 - total_size
+required_space = 30000000 - available_space
+
+potential = []
+for dir, size in fs.items():
+    if (size < 100000):
+        ans += size
+    if size >= required_space:
+        potential.append(size)
+
+ans2 = min(potential)
 
 print(ans)
 print(ans2)
