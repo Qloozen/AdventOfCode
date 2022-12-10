197268204e44892e9e5fbda14595d57fd61384e2
Qiang Loozen <qiangloozen@gmail.com>
2022-12-10 14:59:41 +0100

    day 10 - 2022 to be refactored

--- 2022/day-10/solution.py
+++ 2022/day-10/solution.py
@@ -1,11 +1,68 @@
 ans = 0
 ans2 = 0
 with open("day-10/input.txt") as f:
-    input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+    input = [line.split() for line in f.read().splitlines()]
 
+x = 1
+cycle_count = 0
+strength_list = []
+for command in input:
+    type = command[0]
+    value = int(command[1]) if len(command) > 1 else 0
+    if type == 'noop':
+        if (cycle_count + 1) % 40 == 20:
+            t = x * (cycle_count + 1)
+            strength_list.append(t)
+        cycle_count += 1
+    elif type == 'addx':
+        if (cycle_count + 1) % 40 == 20:
+            t = x * (cycle_count + 1)
+            strength_list.append(t)
 
-print(ans)
-print(ans2)
+        cycle_count += 2
+
+        if cycle_count % 40 == 20:
+            t = x * cycle_count
+            strength_list.append(t)
+
+        x += value
+
+print(sum(strength_list[:6]))
+
+x = 1
+cycle_count = 0
+grid = []
+base_line = ['.' for _ in range(40)]
+line = base_line.copy()
+for command in input:
+    type = command[0]
+    value = int(command[1]) if len(command) > 1 else 0
+    sprite = (x-1, x, x+1)
+    if type == 'noop':
+        if cycle_count in sprite:
+            line[cycle_count] = '#'
+        cycle_count += 1
+        if cycle_count == 40:
+            grid.append(''.join(line))
+            line = base_line.copy()
+            cycle_count = 0
+    elif type == 'addx':
+        if cycle_count in sprite:
+            line[cycle_count] = '#'
+        cycle_count += 1
+        if cycle_count == 40:
+            grid.append(''.join(line))
+            line = base_line.copy()
+            cycle_count = 0
+
+        if cycle_count in sprite:
+            line[cycle_count] = '#'
+        cycle_count += 1
+        if cycle_count == 40:
+            grid.append(''.join(line))
+            line = base_line.copy()
+            cycle_count = 0
+        x += value
+
+for line in grid:
+    print(line)
