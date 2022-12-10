9fc3e4c6790feee72ef09c543c46bf4f59839ae7
Qiang Loozen <qiangloozen@gmail.com>
2022-12-10 13:51:50 +0100

    day 9 - 2022 refactor

--- 2022/day-09/solution.py
+++ 2022/day-09/solution.py
@@ -1,138 +1,39 @@
-ans = 0
-ans2 = 0
 with open("day-09/input.txt") as f:
     input = [tuple(x.split()) for x in f.read().splitlines()]
-    # print(input)
 
-pos = {'h_x': 0, 'h_y': 0, 't_x': 0, 't_y': 0}
-visited = set()
-visited.add((0, 0))
+directions = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)} # replacement for big if else statement when moving the head
+knots = [[0, 0] for _ in range(10)] # knots [0] == head, knots[8] == last tail
+visited = [set() for _ in range(9)] # holds history for every trailing knot
 
-
-def is_adjacent() -> bool:
-    return abs(pos['h_x'] - pos['t_x']) <= 1 and abs(pos['h_y'] - pos['t_y']) <= 1
+def is_adjacent(h_pos, t_pos) -> bool:
+    return abs(h_pos[0] - t_pos[0]) <= 1 and abs(h_pos[1] - t_pos[1]) <= 1
 
 
-def move_left():
-    global pos, visited
-    start = pos['t_x']
-    stop = pos['h_x']
-    visited_coords = set()
-    for x in range(start, stop, -1):
-        print((x, pos['t_y']), ' ->', (pos['h_x'], pos['h_y']))
-        visited_coords.add((x, pos['t_y'])) 
+def move(h_pos, t_pos):
+    if is_adjacent(h_pos, t_pos): return t_pos
 
-    visited.update(visited_coords)
-    pos['t_x'] = stop+1
+    # move tail horizontally
+    if t_pos[0] < h_pos[0]: t_pos[0] += 1 # move right
+    elif t_pos[0] > h_pos[0]: t_pos[0] -= 1 # move left
 
-
-def move_right():
-    global pos, visited
-    start = pos['t_x']
-    stop = pos['h_x']
-    visited_coords = set()
-    for x in range(start, stop):
-        print((x, pos['t_y']), ' ->', (pos['h_x'], pos['h_y']))
-        visited_coords.add((x, pos['t_y']))
-
-    visited.update(visited_coords)
-    pos['t_x'] = stop-1
-
-
-def move_up():
-    global pos, visited
-    start = pos['t_y']
-    stop = pos['h_y']
-    visited_coords = set()
-    for y in range(start, stop):
-        print((pos['t_x'], y), ' ->', (pos['h_x'], pos['h_y']))
-        visited_coords.add((pos['t_x'], y))
-
-    visited.update(visited_coords)
-    pos['t_y'] = stop-1
-
-
-def move_down():
-    global pos, visited
-    start = pos['t_y']
-    stop = pos['h_y']
-    visited_coords = set()
-    for y in range(start, stop, -1):
-        print((pos['t_x'], y), ' ->', (pos['h_x'], pos['h_y']))
-        visited_coords.add((pos['t_x'], y))
-
-    visited.update(visited_coords)
-    pos['t_y'] = stop+1
-
+    # move tail vertically
+    if t_pos[1] < h_pos[1]: t_pos[1] += 1 # move up
+    elif t_pos[1] > h_pos[1]: t_pos[1] -= 1 # move down
+    return t_pos
 
 for command in input:
     direction = command[0]
     steps = int(command[1])
-    print(command, ' begin head', (pos['h_x'], pos['h_y']))
-    if direction == 'L':
-        pos['h_x'] -= steps
-        if is_adjacent():
-            continue
-        if pos['h_y'] == pos['t_y']:
-            move_left()
-        else:
-            #  head is 1 above or 1 under
-            pos['t_y'] = pos['h_y']
-            pos['t_x'] -= 1
-            move_left()
-    if direction == 'R':
-        pos['h_x'] += steps
-        if is_adjacent():
-            continue
-        if pos['h_y'] == pos['t_y']:
-            move_right()
-        else:
-            #  head is 1 above or 1 under
-            pos['t_y'] = pos['h_y']
-            pos['t_x'] += 1
-            move_right() 
-    if direction == 'U':
-        pos['h_y'] += steps
-        if is_adjacent():
-            continue
-        if pos['h_x'] == pos['t_x']:
-            move_up()
-        else:
-            #  head is 1 left or 1 right
-            pos['t_x'] = pos['h_x']
-            pos['t_y'] += 1
-            move_up()
-    if direction == 'D':
-        pos['h_y'] -= steps
-        if is_adjacent():
-            continue
-        if pos['h_x'] == pos['t_x']:
-            move_down()
-        else:
-            #  head is 1 left or 1 right
-            pos['t_x'] = pos['h_x']
-            pos['t_y'] -= 1
-            move_down()
+    for i in range(abs(steps)):
+        # move front knot
+        knots[0][0] += directions[direction][0]
+        knots[0][1] += directions[direction][1]
+        
+        # every next knot will be the head of the following knot, it makes the same move
+        for knot in range(9):  
+            leading_knot, next_knot = knots[knot], knots[knot + 1]
+            next_coords = move(leading_knot, next_knot)
+            visited[knot].add(tuple(next_coords))
 
-print(len(visited))
-
-max_x = max(x for x, y in visited) + 1
-max_y = max(y for x, y in visited)
-
-# create a 2D grid with dots
-grid = []
-for y in range(max_y + 1):
-    row = []
-    for x in range(max_x + 1):
-        if x==0 and y==0:
-            row.append('s')
-        elif (x, y) in visited:
-            row.append("#")
-        else:
-            row.append(".")
-    grid.append(row)
-
-grid.reverse()
-# print the grid
-for row in grid:
-    print(" ".join(row))
+print(len(visited[1]))
+print(len(visited[8]))
