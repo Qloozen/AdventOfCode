edb009500c791f30e3573911ff18b248e074ff4f
Qiang Loozen <qiangloozen@gmail.com>
2022-12-11 15:33:22 +0100

    day 11 - 2022

--- 2022/day-11/solution.py
+++ 2022/day-11/solution.py
@@ -1,42 +1,21 @@
-ans = 0
-ans2 = 0
-import re
-from typing import List
-with open("day-11/input.txt") as f:
-    input = [monkey.splitlines() for monkey in  f.read().split('\n\n')]
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
-
 class Monkey:
     def __init__(self, items, operation, test, true, false):
-        self.items = [int(i) for i in items.split(', ')]
-        self.operation = operation.split('new = ')[1]
-        self.test = int(test.split('by ')[1])
-        self.true = int(true.split('monkey ')[1])
-        self.false = int(false.split('monkey ')[1])
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.true = true
+        self.false = false
         self.inspection_count = 0
     def __throw(self, worry_level) -> int:
-        if worry_level % self.test == 0:
-            # print('     Current worry level is divisible by '+ str(self.test))
-            return self.true
-        else:
-            # print('     Current worry level is divisible by '+ str(self.test))
-            return self.false
+        return self.true if worry_level % self.test == 0 else self.false
 
-        # return self.true if worry_level % self.test == 0 else self.false
-
-    def inspect(self):
+    def inspect(self, mod):
         self.inspection_count += 1
         item = self.items.pop(0)
-        # print(' Monkey inspects an item with a worry level of ' + str(item))
-        evaluation = self.get_eval(item)
-        worry_level = eval(evaluation)
-        # print('     Worry level = ' + evaluation + ' = ' + str(worry_level))
-        worry_level //= 3
-        # print('     Monkey gets bored with item. Worry level is divided by 3 to ' + str(worry_level))
+        worry_level = eval(self.get_eval(item))
+        # part 1: divide by 3, part 2: take modulo instead
+        worry_level = worry_level // 3 if mod == None else worry_level % mod
         next_monkey = self.__throw(worry_level)
-        # print("     Item with worry level " + str(worry_level) +  ' is thrown to monkey ' + str(next_monkey))
         return (next_monkey, worry_level)
 
     def get_eval(self, worry_level):
@@ -44,36 +23,51 @@ class Monkey:
 
     def add_item(self, item):
         self.items.append(item)
-    def __str__(self) -> str:
-        return ', '.join(str(x) for x in self.items)
 
+    def reset_items(self, items):
+        self.items = items
 
+    def reset_inspection_count(self):
+        self.inspection_count = 0
 
-monkeys: List[Monkey] = []
-for monkey_input in input:
-    items = monkey_input[1].strip().split('Starting items: ')[1]
-    operation = monkey_input[2].strip().split('Operation: ')[1]
-    test = monkey_input[3].strip().split('Test: ')[1]
-    true = monkey_input[4].strip().split('If true: ')[1]
-    false = monkey_input[5].strip().split('If false: ')[1]
-    monkey = Monkey(items, operation, test, true, false)
-    monkeys.append(monkey) 
+from functools import reduce
+from typing import List
+with open("day-11/input.txt") as f:
+    input = [monkey.splitlines() for monkey in  f.read().split('\n\n')]
+    monkeys: List[Monkey] = []
+    for monkey_input in input:
+        items = [int(i) for i in monkey_input[1].split(': ')[1].split(', ')]
+        operation = monkey_input[2].split('= ')[1]
+        test = int(monkey_input[3].split('by ')[1])
+        true = int(monkey_input[4].split('monkey ')[1])
+        false = int(monkey_input[5].split('monkey ')[1])
+        monkey = Monkey(items, operation, test, true, false)
+        monkeys.append(monkey) 
 
+modulo = 1 # used in part 2
+modulo = reduce(lambda x, y: x * y, [monkey.test for monkey in monkeys]) 
+
+# part 1
 for i in range(20):
     for m in range(len(monkeys)):
         monkey = monkeys[m]       
-        # print("Monkey " + str(m))
         for _ in range(len(monkey.items)):
-            next_monkey, worry_level = monkey.inspect()
+            next_monkey, worry_level = monkey.inspect(None)
             monkeys[next_monkey].add_item(worry_level)
-    print("After round: " + str(i + 1))
+two_most_active = list(sorted([monkey.inspection_count for monkey in monkeys]))[-2:]
+print(two_most_active[0] * two_most_active[1])
+
+# part 2
+for i in range(len(input)): # resetting monkeys
+    monkey_input = input[i]
+    monkeys[i].reset_items([int(i) for i in monkey_input[1].split(': ')[1].split(', ')])
+    monkeys[i].reset_inspection_count()
+
+for i in range(10000): # this one will take a couple of seconds
     for m in range(len(monkeys)):
         monkey = monkeys[m]       
-        # print("Monkey " + str(m) + ': ' + monkey.__str__())
-        print("Monkey " + str(m) + ' inspected items ' + str(monkey.inspection_count) + ' times.')
-
-
+        for _ in range(len(monkey.items)):
+            next_monkey, worry_level = monkey.inspect(modulo)
+            monkeys[next_monkey].add_item(worry_level)
 two_most_active = list(sorted([monkey.inspection_count for monkey in monkeys]))[-2:]
-
 print(two_most_active[0] * two_most_active[1])
-print(ans2)
