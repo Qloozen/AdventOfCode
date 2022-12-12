b12611b18cf0f8ed122ae02226875dcf2988d8e7
qloozen <qiangloozen@gmail.com>
2022-12-12 11:07:55 +0100

    day 12 - 2022

--- 2022/day-12/README.md
+++ 2022/day-12/README.md
@@ -1,1 +1,15 @@
-# [Day 1: Calorie Counting](https://adventofcode.com/2022/day/1)
+# [Day 12: Hill Climbing Algorithm](https://adventofcode.com/2022/day/12)
+
+You try contacting the Elves using your handheld device, but the river you're following must be too low to get a decent signal.
+
+You ask the device for a heightmap of the surrounding area (your puzzle input). The heightmap shows the local area from above broken into a grid; the elevation of each square of the grid is given by a single lowercase letter, where a is the lowest elevation, b is the next-lowest, and so on up to the highest elevation, z.
+
+Also included on the heightmap are marks for your current position (S) and the location that should get the best signal (E). Your current position (S) has elevation a, and the location that should get the best signal (E) has elevation z.
+
+You'd like to reach E, but to save energy, you should do it in as few steps as possible. During each step, you can move exactly one square up, down, left, or right. To avoid needing to get out your climbing gear, the elevation of the destination square can be at most one higher than the elevation of your current square; that is, if your current elevation is m, you could step to elevation n, but not to elevation o. (This also means that the elevation of the destination square can be much lower than the elevation of your current square.)
+
+--- Part Two ---
+
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
--- 2022/day-12/solution.py
+++ 2022/day-12/solution.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, row, col, dist):
+    def __init__(self, row, col, step):
         self.row = row
         self.col = col
-        self.dist = dist
+        self.step = step
     
-s = Node(0, 0 ,0)
+starting_nodes = []
 
 with open("day-12/input.txt") as f:
     grid = []
@@ -12,6 +12,51 @@ with open("day-12/input.txt") as f:
         row = []
         for char in line:
             if char == 'S':
-                s = (len(grid), len(row))
-            row.append(char)
+                starting_nodes.insert(0, Node(len(grid), len(row), 0))
+                row.append('a')
+            elif char == 'a':
+                starting_nodes.append(Node(len(grid), len(row), 0))
+                row.append(char)
+            else:
+                row.append(char)
         grid.append(row)
+
+def find_shortest_path(s):
+    queue = [s]
+    visited = set()
+    visited.add((0,0))
+    while len(queue) != 0:
+        c = queue.pop(0)
+
+        if grid[c.row][c.col] == "E":
+            return c.step
+
+        if (can_move(c.row, c.col-1, c, visited)): # left
+            queue.append(Node(c.row, c.col-1, c.step+1))
+        if (can_move(c.row, c.col+1, c, visited)): # right
+            queue.append(Node(c.row, c.col+1, c.step+1))
+        if (can_move(c.row-1, c.col, c, visited)): # up
+            queue.append(Node(c.row-1, c.col, c.step+1))
+        if (can_move(c.row+1, c.col, c, visited)): # down
+            queue.append(Node(c.row+1, c.col, c.step+1))
+
+
+
+def can_move(y, x, c, visited):
+    if (x < 0 or x > len(grid[0])-1) or (y < 0 or y > len(grid)-1):
+        return False
+    if (ord(grid[y][x]) - ord(grid[c.row][c.col]) > 1):
+        return False
+    if (y, x) in visited:
+        return False
+    
+    visited.add((y, x))
+    return True
+
+
+print(find_shortest_path(starting_nodes[0])) 
+all_paths = [find_shortest_path(s) for s in starting_nodes]
+print(min(filter(lambda x: x is not None, all_paths)))
+
+# Your puzzle answer was 497
+# Your puzzle answer was 492
