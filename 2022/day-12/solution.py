c0b405a9ebf435892b80246ab17f5088de53511c
Qiang Loozen <qiangloozen@gmail.com>
2022-12-12 07:00:58 +0100

    day 12 - 2022 (parsed input)

--- 2022/day-12/solution.py
+++ 2022/day-12/solution.py
@@ -1,11 +1,17 @@
-ans = 0
-ans2 = 0
+class Node:
+    def __init__(self, row, col, dist):
+        self.row = row
+        self.col = col
+        self.dist = dist
+    
+s = Node(0, 0 ,0)
+
 with open("day-12/input.txt") as f:
-    input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
-
-
-print(ans)
-print(ans2)
+    grid = []
+    for line in f.read().splitlines():
+        row = []
+        for char in line:
+            if char == 'S':
+                s = (len(grid), len(row))
+            row.append(char)
+        grid.append(row)
