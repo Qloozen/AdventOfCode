0e90dcc31516584c4b878de07b3c07d15f8eddf6
Qiang Loozen <qiangloozen@gmail.com>
2022-12-13 14:00:58 +0100

    day 13 - 2022

--- 2022/day-13/README.md
+++ 2022/day-13/README.md
@@ -1,1 +1,20 @@
-# [Day 1: Calorie Counting](https://adventofcode.com/2022/day/1)
+# [Day 13: Distress Signal](https://adventofcode.com/2022/day/13)
+(this is only the context of the puzzle)
+
+You climb the hill and again try contacting the Elves. However, you instead receive a signal you weren't expecting: a distress signal.
+
+Your handheld device must still not be working properly; the packets from the distress signal got decoded out of order. You'll need to re-order the list of received packets (your puzzle input) to decode the message.
+
+Your list consists of pairs of packets; pairs are separated by a blank line. You need to identify how many pairs of packets are in the right order.
+
+Packet data consists of lists and integers. Each list starts with [, ends with ], and contains zero or more comma-separated values (either integers or other lists). Each packet is always a list and appears on its own line.
+
+--- Part Two ---
+
+Now, you just need to put all of the packets in the right order. Disregard the blank lines in your list of received packets.
+
+The distress signal protocol also requires that you include two additional divider packets:
+
+[[2]]
+[[6]]
+Using the same rules as before, organize all packets - the ones in your list of received packets as well as the two divider packets - into the correct order.
--- 2022/day-13/solution.py
+++ 2022/day-13/solution.py
@@ -1,11 +1,50 @@
 ans = 0
-ans2 = 0
 with open("day-13/input.txt") as f:
-    input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+    input = [[eval(packet) for packet in pair.splitlines()] for pair in f.read().split('\n\n')]
+with open("day-13/input.txt") as f:
+    input2 = [eval(packet) for packet in f.read().replace('\n\n', '\n').splitlines()]
+    divider_a, divider_b = [[2]], [[6]]
+    input2.extend([divider_a, divider_b]) 
 
 
+def compare_lists(a, b):
+    compare = zip(a, b)
+    for left, right in compare:
+        if type(left) == list or type(right) == list:
+            next_a = [left] if type(left) == int else left
+            next_b = [right] if type(right) == int else right
+            res = compare_lists(next_a, next_b)
+            if res == None: continue
+            return res
+        else:
+            if left < right :
+                return True
+            if right < left :
+                return False
+    if len(a) == len(b):
+        return None 
+    return len(a) < len(b)
+
+
+# part 1
+for i in range(len(input)):
+    is_valid = compare_lists(input[i][0], input[i][1])
+    if is_valid: ans += i+1
 print(ans)
-print(ans2)
+
+# part 2
+def bubble_sort(unordered_list) -> bool:
+    all_sorted = True
+    for i in range(len(unordered_list)-1):
+        is_valid = compare_lists(unordered_list[i], unordered_list[i+1])
+        if not is_valid:
+            unordered_list[i], unordered_list[i+1] = unordered_list[i+1], unordered_list[i]
+            all_sorted = False
+    return all_sorted
+
+unordered_list = input2
+needs_sorting = True
+while needs_sorting:
+    needs_sorting = not bubble_sort(unordered_list)
+
+print((unordered_list.index(divider_a)+1) * (unordered_list.index(divider_b) + 1))
