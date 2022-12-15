0da05c0149174495b73a7d0fe632529165097443
Qiang Loozen <qiangloozen@gmail.com>
2022-12-15 15:36:16 +0100

    day 15 - 2022 (part1)

--- 2022/day-15/solution.py
+++ 2022/day-15/solution.py
@@ -2,10 +2,52 @@ ans = 0
 ans2 = 0
 with open("day-15/input.txt") as f:
     input = f.read().splitlines()
-    # input = f.read().split("")
-    # input = [int(num) for num in f.read().splitlines()]
-    # input = [[int(num) for num in line] for line in f.read().splitlines()]
+    data = []
+    all_beacons = set()
+    all_sensors = set()
+    target_y=2000000
+    for line in input:
+        sx = int(line.split('x=')[1].split(',')[0])
+        sy = int(line.split('y=')[1].split(":")[0])
+        bx = int(line.split('x=')[2].split(',')[0])
+        by = int(line.split('y=')[2].split(":")[0])
+        data.append([(sy, sx), (by, bx)])
+        if by == target_y:
+            all_beacons.add((by, bx))
+        if sy == target_y:
+            all_sensors.add((sy, sx))
 
+def find_detected(sensor, beacon, detected):
+    sy, sx = sensor
+    by, bx = beacon
+    dist = abs(sx- bx) + abs(sy- by)
+
+    top_index = sy-dist 
+    bottom_index = sy+dist 
+
+    in_range = target_y >= top_index and target_y <= bottom_index
+
+    if in_range:
+        steps = abs(sy-target_y)
+        if steps == 0:
+            detected.add((sx-dist, sx+dist))
+        a = sx - (dist-steps)
+        b = sx + (dist-steps)
+        detected.add((a, b))
+
+detected = set()
+
+for line in data:
+    sensor, beacon = line[0], line[1]
+    find_detected(sensor, beacon, detected)
+
+
+lowest = min(range[0] for range in detected)
+higest = max(range[1] for range in detected)
+    
+objects = len(all_beacons) + len(all_sensors)
+
+diff = abs(lowest - higest)+1
+ans = diff - objects
 
 print(ans)
-print(ans2)
