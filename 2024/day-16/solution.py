9f1de9a22bd2dbdbf6e5448246e183618aa7c63b
Qloozen <qiangloozen@gmail.com>
2024-12-21 15:36:36 +0100

    2023-day-16 p1

--- 2024/day-16/solution.py
+++ 2024/day-16/solution.py
@@ -1,4 +1,9 @@
+
+import heapq
+from collections import namedtuple
 with open("input.txt") as f:
+    Step = namedtuple("Step", "cost y x dir")
+
 
     start = (0, 0)
     grid = []
@@ -9,47 +14,42 @@ with open("input.txt") as f:
         grid.append(list(line))
     R = len(grid)
     C = len(grid[0])
-
-    print(grid[start[0]][start[1]])
-    print(start)
     
     
     def bfs(start):
         seen = set()
-        q = [(start[0], start[1], 0, 1, 0)] # y, x, dist,  prev_d,  turns
+        q = [Step(cost=0, y=start[0], x=start[1], dir=1)]
         scores = []
 
         while len(q) > 0:
-            # print(len(q))
-            pos = q.pop(0)
-            y, x, dist, prev_d, turns = pos
+            pos = heapq.heappop(q)
+            cost, y, x, prev_d = pos
 
             if grid[y][x] == "E":
                 print(pos)
-                scores.append(1000*turns+dist)
+                scores.append(cost)
                 continue
-
+            
+            if (y, x, prev_d) in seen: continue
             
             seen.add((y, x, prev_d))
 
-            for d_i, (dy, dx) in enumerate([(-1, 0), (0, 1), (1, 0), (0, -1)]): # 0: north, 1: east, 2: south, 3: west
+            for d_i, (dy, dx) in enumerate([(-1, 0), (0, 1), (1, 0), (0, -1)]): 
                 ny, nx = y + dy, x + dx
                 if not(0 <= ny < R and 0 <= nx < C): continue
                 if grid[ny][nx] == "#": continue
 
-                next_turns = turns + (1 if d_i != prev_d else 0)
-                next_pos = (ny, nx, dist + 1, d_i, next_turns)
-                if (ny, nx, d_i) in seen and grid[ny][nx] != "E": continue
+                new_cost = cost + (1000 if d_i != prev_d else 0)
+                new_cost += 1
+                next_pos = Step(cost=new_cost, y=ny, x=nx, dir=d_i)
                 if next_pos in q: continue
-                q.append(next_pos)
+                heapq.heappush(q, next_pos)
         return sorted(scores)
-
-
+    
     sorted_scores = bfs(start)
-    print(sorted_scores)
+    # print(sorted_scores)
     print(sorted_scores[0])
 
-    # too high 99432
-    # not 97432 too high
+# Your puzzle answer was 89460.
         
 
