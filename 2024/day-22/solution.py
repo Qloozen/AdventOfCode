a7fe739a2d59af8caa85c126f94d1365d6fc0458
Qloozen <qiangloozen@gmail.com>
2024-12-22 15:09:39 +0100

    2024-day-22 p1

--- 2024/day-22/solution.py
+++ 2024/day-22/solution.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-with open("ex2.txt") as f:
+with open("input.txt") as f:
     intial_values = list(map(int, f.read().splitlines()))
 
     def get_secrets(initial_secret, n):
@@ -43,26 +43,18 @@ with open("ex2.txt") as f:
         for k, v in current_sequences.items():
             sequences[k] += v
 
-    # print(p1)
+    print(p1)
 
     highest_value = 0
     for k, v in sequences.items(): 
         if v > highest_value:
             highest_value = v
             # print(k)
-    # print(highest_value)
+    print(highest_value)
 
     # Your puzzle answer was 13461553007.
     # 1550 too high
 
-
-    next_secrets = get_secrets(123, 9)
-    current_sequences = defaultdict(int)
-    ones = list(map(lambda x: x % 10, next_secrets))
-    changes = [a - b for a, b in zip(ones[1:], ones)]
-    for i in range(len(changes) - 4):
-        seq_key = tuple(changes[i:i+4])
-        current_sequences[seq_key] = max(current_sequences[seq_key], ones[i+4])
 
 
 
