83cdffbc53d4a26ce01ebeccebe2b401d172f166
Qloozen <qiangloozen@gmail.com>
2024-12-24 20:17:31 +0100

    2024-day-12

--- 2024/day-12/readme.md
+++ 2024/day-12/readme.md
@@ -4,16 +4,23 @@
 
 ### Approach
 
-I've done part one with a queue, but this could also be done with some recursion. For each plant in a region I simply determine if the directions: up, right, down, left are out of bounds or does not contain the same plant. Then we know that place should be a fence.
+I've done part one with a queue, but this could also be done with some recursion. For each plant in a region I simply determine if the directions: up, right, down, left are out of bounds or does not contain the same plant. Then we know that place should be a fence. I also return the visited set of each region. This is nice to have for part 2.
 
 ## Part 2
 
 > In the second part the cost is calculated by multiplying the area by the number of sides.
 
-### Approach (not finished)
+### Approach
 
-My approach was to find the number of corners. Corners are plants that have at least 2 fences next to it. 2 fences would equal to 1 corner, 3 fences would equal to 2 corners and 4 fences would equal to 4 corners. We also need to find the inward corners. To find inward corners I store the plants around the current region. The amount of times a plant is found in the surrounding area is the amount of fences it touches. This also means 2 fences = 1 side, 3 fences = 2 sides and 4 fences = 4 sides.
+You can state that the number of sides is equal to the number of corners. If a plant (square) has 2 fences it either forms a corner or forms 2 parallel fences (which is not a valid corner). `ex3.txt` helped me to find this issue. If a plant has 3 fences it is sticking our of its region, forming 2 corners. If a plant has 4 fences, then the plant itself is the region, so it has 4 corners. So these rules I followed:
 
-There are two catches. The first one is that 2 fences does not always equal 1 corner. If the region is one wide or tall, it could mean the fences are parralel from eachother, so this would not count as a corner.
+- 1 fence = 0 corners (part of a straight line)
+- 2 fences = 1 corner if not parallel
+- 3 fences = 2 corners
+- 4 fences = 4 corners
 
-The second catch is also with 2 fences. If there is a region in another region, there could be a corner in the middle of the region. This would conflict with counting as a corner and a inwards corner, resulting in a too large number of sides. Still need this fix this one..
+So one fail case were the parallel fences, which I fixed by checking if the direction of the fences are not opposite. The other fail case was on example `ex4.txt`. Here you have two B regions inside the A region. The problem arises when two A plants are diagonally connected _in the center_, both with a B plant next to it. Both A plants and B plant (diagonally of eachother) will count 2 corners for region A, causing 2 extra corners.
+
+To fix this I keep track of diagonal connections. And removing 2 corners of a region if it has a diagonal connection with a plant in the same region. One small error I made was to not include a diagonal block multiple times, so if a plant has 2 or more diagonal connections, it didn't remove 2 corners for each pair of diagonals. I found this with the `edgecase.txt` example.
+
+_No, I am not going to optimize this solution.._
--- 2024/day-12/solution.py
+++ 2024/day-12/solution.py
@@ -1,42 +1,139 @@
-import re
-from functools import reduce
-from collections import defaultdict
-
-# So, it has a total price of 1930.
+from collections import deque
 
 with open("input.txt") as f:
-    lines = [line for line in f.read().splitlines()]
-    R = len(lines)
-    C = len(lines[0])
+    grid = [line for line in f.read().splitlines()]
+    R = len(grid)
+    C = len(grid[0])
     p1 = 0
+    dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    def bfs(pos, grid):
+        # part 1
+        q = deque([pos])
+        area = 0
+        perimeter = 0
+        region = set()
+
+        while len(q) > 0:
+            y, x = q.popleft()
+            c = grid[y][x]
+
+            if (y, x) in region: continue
+            region.add((y, x))
+            area += 1
+            for dy, dx in dirs:
+                ny, nx = y + dy, x + dx
+                if not (0 <= ny < R and 0 <= nx < C): 
+                    perimeter += 1
+                    continue
+                if grid[ny][nx] != c:
+                    perimeter += 1
+                    continue
+                if grid[ny][nx] == c and not (ny, nx) in region: 
+                    q.append((ny, nx))
+        return (area, perimeter, region)
+    
+    def get_inward_fences(pos, region):
+        # part 2
+        y, x = pos
+        inward_fences = [] 
+
+        for dy, dx in dirs:
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < R and 0 <= nx < C): continue
+            if (ny, nx) in region: 
+                inward_fences.append((ny, nx))
+        return inward_fences if len(inward_fences) > 1 else []
+    
+    def parallel_pos(fences):
+        # part 2
+        if len(fences) != 2 : return False
+        a, b, = fences
+
+        return abs(a[0] - b[0]) == 2 or abs(a[1] - b[1]) == 2
+
+    def get_corners(fences):
+        # part 2
+        connected_fences = len(fences)
+        if connected_fences < 2: return 0
+        if connected_fences == 2: return 0 if parallel_pos(fences) else 1
+        if connected_fences == 3: return 2
+        if connected_fences == 4: return 4
+
+    def check_diagonal(pos, grid, outward_fences): 
+        # part 2
+        y, x = pos
+        c = grid[y][x]
+        diagonals = []
+        for dy, dx in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < R and 0 <= nx < C) or grid[ny][nx] != c: continue
+            other_fences = []
+
+            for ddy, ddx in dirs:
+                nny, nnx = ny + ddy, nx + ddx
+                if not (0 <= nny < R and 0 <= nnx < C): continue
+                if grid[nny][nnx] != c:
+                    other_fences.append((nny, nnx))
+            if len(set(outward_fences) & set(other_fences)) == 2: 
+                diagonals.append((ny, nx))
+        
+        return (False, None) if len(diagonals) == 0 else (True, diagonals)
+    
+    def calculate_sides(region, grid):
+        # part 2
+        visited_outside = set()
+        total_corners = 0
+        diagonals = []
+
+        for plant in region:
+            y, x = plant
+            c = grid[y][x]
+
+            outward_fences = []
+            corners = 0
+            for dy, dx in dirs:
+                ny, nx = y + dy, x + dx
+                if not (0 <= ny < R and 0 <= nx < C): 
+                    outward_fences.append((ny, nx))
+                    continue
+                if grid[ny][nx] != c:
+                    outward_fences.append((ny, nx))
+                    
+                    if (ny, nx) not in visited_outside:
+                        inward_fences = get_inward_fences((ny, nx), region)
+                        corners += get_corners(inward_fences)
+                        visited_outside.add((ny, nx))
+                    continue
+            
+            is_diagonal, diagonal_with = check_diagonal((y, x), grid, outward_fences)
+            if is_diagonal:
+                diagonals.append((y, x)) 
+                for diagonal in diagonal_with:
+                    if diagonal in diagonals:
+                        total_corners -= 2
+
+            outer_corners = get_corners(outward_fences)  
+            corners += outer_corners
+            total_corners += corners
+
+        return total_corners
 
     seen = set()
+    p2 = 0
     for i in range(R):
         for j in range(C):
             if (i, j) in seen: continue
-            q = [(i, j)]
-            area = 0
-            perimeter = 0
-
-            while len(q) > 0:
-                y, x = q.pop(0)
-                c = lines[y][x]
-
-                seen.add((y, x))
-                area += 1
-                for dy, dx in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                    ny, nx = y + dy, x + dx
-                    if not 0 <= ny < R:
-                        perimeter += 1
-                        continue
-                    if not 0 <= nx < C:
-                        perimeter += 1
-                        continue
-                    if lines[ny][nx] != c:
-                        perimeter += 1
-                        continue
-                    if lines[ny][nx] == c and not (ny, nx) in seen and (ny, nx) not in q:
-                        q.append((ny, nx))
+            area, perimeter, region = bfs((i, j), grid)
             p1 += area * perimeter
-            # print(c, area, perimeter)
+            seen |= region
+
+            # Part 2
+            sides = calculate_sides(region, grid)
+            p2 += area * sides
     print(p1)
+    print(p2)
+
+    # Your puzzle answer was 1381056.
+    # Your puzzle answer was 834828.
+
